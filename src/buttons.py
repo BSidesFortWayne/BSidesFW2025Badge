9b416f584837fbc28df2415888e159c3a594b6da
luke-gilsinger <lukealt@gilsinger.net>
2025-02-15 10:03:17 -0500

    Debounce buttons

--- src/buttons.py
+++ src/buttons.py
@@ -1,9 +1,12 @@
 from machine import Pin
+import time
 
 class Buttons:
     def __init__(self):
         self.button_io = [0, 33, 35, 34]
         self.button_objects = []
+        self.debounce_time = 20
+        self.last_press = 0
         self.button_callback = lambda: None
 
         for index, pin in enumerate(self.button_io):
@@ -13,6 +16,8 @@ class Buttons:
     def button_handler(self, pin):
             # find standard button number
             button = self.button_objects.index(pin)+1
-
+            if self.last_press+self.debounce_time >= time.ticks_ms(): # debounce
+                return
             print(f'Button {button}, pin {pin} was pressed.')
             self.button_callback(button)
+            self.last_press = time.ticks_ms()
