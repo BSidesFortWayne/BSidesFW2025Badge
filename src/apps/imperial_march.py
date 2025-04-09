5022b79693c0ea5c15d4f6b5cda5dfc52d9e7a98
David Prows <david@prowshouse.com>
2025-04-08 20:26:11 -0400

    update imperial march

--- src/apps/imperial_march.py
+++ src/apps/imperial_march.py
@@ -2,124 +2,97 @@ from machine import Pin, PWM
 from time import sleep_ms
 from apps.app import BaseApp
 
-class Imperial_March(BaseApp):
+class ImperialMarch(BaseApp):
     name = "Imperial March"
-    def __init__(self, buzzer_pin=12, bpm=120):
+    def __init__(self, buzzer_pin=15, bpm=100):
         self.bpm = bpm
-        self.Q = int(60000 / self.bpm)
+        self.Q = int(60000 / bpm)
         self.H = 2 * self.Q
         self.E = self.Q // 2
         self.S = self.Q // 4
-        self.W = 4 * self.Q
 
         self.buzzer = PWM(Pin(buzzer_pin))
         self.buzzer.duty(0)
 
         self.notes = {
-            'C4': 261.63, 'Db4': 277.18, 'D4': 293.66, 'Eb4': 311.13, 'E4': 329.63,
-            'F3': 174.61, 'F4': 349.23, 'Gb4': 369.99, 'G4': 392.00,
-            'Ab3': 207.65, 'Ab4': 415.30, 'LA3': 220.00, 'LA4': 440.00,
-            'Bb3': 233.08, 'Bb4': 466.16, 'B3': 246.94, 'C5': 523.25,
-            'Eb4': 311.13, 'D4': 293.66, 'Db4': 277.18, 'C4': 261.63
+            'A3': 220, 'A4': 440, 'A5': 880,
+            'C4': 262, 'C5': 523, 'C#4': 277, 'C#5': 554,
+            'D4': 294, 'D5': 587,
+            'E4': 330, 'E5': 659,
+            'F4': 349, 'F5': 698,
+            'G4': 392, 'G5': 784,
+            'G#4': 415, 'G#5': 831,
+            'REST': 0
         }
 
-    def mytone(self, freq, duration):
+        # Full Imperial March melody, with corresponding note durations
+        self.melody = [
+            ('A4', self.Q), ('A4', self.Q), ('A4', self.Q),
+            ('F4', self.E), ('C5', self.S),
+            ('A4', self.Q), ('F4', self.E), ('C5', self.S), ('A4', self.H),
+
+            ('E5', self.Q), ('E5', self.Q), ('E5', self.Q),
+            ('F5', self.E), ('C5', self.S),
+            ('G#4', self.Q), ('F4', self.E), ('C5', self.S), ('A4', self.H),
+
+            # Pause after first section
+            ('REST', self.H),
+
+            ('A5', self.Q), ('A4', self.E), ('A4', self.S),
+            ('A5', self.Q), ('G#5', self.E), ('G5', self.S),
+            ('F5', self.S), ('E5', self.S), ('F5', self.E),
+            ('REST', self.E),
+
+            ('A4', self.E), ('C5', self.Q), ('A4', self.E),
+            ('C5', self.Q), ('E5', self.H),
+
+            # Next section
+            ('A4', self.Q), ('A4', self.Q), ('A4', self.Q),
+            ('F4', self.E), ('C5', self.S),
+            ('A4', self.Q), ('F4', self.E), ('C5', self.S), ('A4', self.H),
+
+            ('E5', self.Q), ('E5', self.Q), ('E5', self.Q),
+            ('F5', self.E), ('C5', self.S),
+            ('G#4', self.Q), ('F4', self.E), ('C5', self.S), ('A4', self.H),
+
+            # Section with variation
+            ('A5', self.Q), ('A4', self.E), ('A4', self.S),
+            ('A5', self.Q), ('G#5', self.E), ('G5', self.S),
+            ('F5', self.S), ('E5', self.S), ('F5', self.E),
+            ('REST', self.E),
+
+            ('A4', self.E), ('C5', self.Q), ('A4', self.E),
+            ('C5', self.Q), ('E5', self.H),
+
+            # Final repeat section
+            ('A4', self.Q), ('A4', self.Q), ('A4', self.Q),
+            ('F4', self.E), ('C5', self.S),
+            ('A4', self.Q), ('F4', self.E), ('C5', self.S), ('A4', self.H),
+
+            ('E5', self.Q), ('E5', self.Q), ('E5', self.Q),
+            ('F5', self.E), ('C5', self.S),
+            ('G#4', self.Q), ('F4', self.E), ('C5', self.S), ('A4', self.H),
+            
+            # End
+            ('REST', self.H),
+        ]
+
+    def tone(self, note, duration):
+        freq = self.notes.get(note, 0)
         if freq > 0:
-            self.buzzer.freq(int(freq))
+            self.buzzer.freq(freq)
             self.buzzer.duty(512)
         else:
-            self.buzzer.duty(0)
+            self.buzzer.duty(0)  # pause
+
         sleep_ms(duration)
         self.buzzer.duty(0)
+        sleep_ms(30)  # small gap between notes
 
     def play(self):
-        n = self.notes
-        Q, H, E, S = self.Q, self.H, self.E, self.S
-
-        self.mytone(n['LA3'], Q)
-        self.mytone(n['LA3'], Q)
-        self.mytone(n['LA3'], Q)
-        self.mytone(n['F3'], E + S)
-        self.mytone(n['C4'], S)
-
-        self.mytone(n['LA3'], Q)
-        self.mytone(n['F3'], E + S)
-        self.mytone(n['C4'], S)
-        self.mytone(n['LA3'], H)
-
-        self.mytone(n['E4'], Q)
-        self.mytone(n['E4'], Q)
-        self.mytone(n['E4'], Q)
-        self.mytone(n['F4'], E + S)
-        self.mytone(n['C4'], S)
-
-        self.mytone(n['Ab3'], Q)
-        self.mytone(n['F3'], E + S)
-        self.mytone(n['C4'], S)
-        self.mytone(n['LA3'], H)
-
-        self.mytone(n['LA4'], Q)
-        self.mytone(n['LA3'], E + S)
-        self.mytone(n['LA3'], S)
-        self.mytone(n['LA4'], Q)
-        self.mytone(n['Ab4'], E + S)
-        self.mytone(n['G4'], S)
-
-        self.mytone(n['Gb4'], S)
-        self.mytone(n['E4'], S)
-        self.mytone(n['F4'], E)
-        sleep_ms(E)
-        self.mytone(n['Bb3'], E)
-        self.mytone(n['Eb4'], Q)
-        self.mytone(n['D4'], E + S)
-        self.mytone(n['Db4'], S)
-
-        self.mytone(n['C4'], S)
-        self.mytone(n['B3'], S)
-        self.mytone(n['C4'], E)
-        sleep_ms(E)
-        self.mytone(n['F3'], E)
-        self.mytone(n['Ab3'], Q)
-        self.mytone(n['F3'], E + S)
-        self.mytone(n['LA3'], S)
-
-        self.mytone(n['C4'], Q)
-        self.mytone(n['LA3'], E + S)
-        self.mytone(n['C4'], S)
-        self.mytone(n['E4'], H)
-
-        self.mytone(n['LA4'], Q)
-        self.mytone(n['LA3'], E + S)
-        self.mytone(n['LA3'], S)
-        self.mytone(n['LA4'], Q)
-        self.mytone(n['Ab4'], E + S)
-        self.mytone(n['G4'], S)
-
-        self.mytone(n['Gb4'], S)
-        self.mytone(n['E4'], S)
-        self.mytone(n['F4'], E)
-        sleep_ms(E)
-        self.mytone(n['Bb3'], E)
-        self.mytone(n['Eb4'], Q)
-        self.mytone(n['D4'], E + S)
-        self.mytone(n['Db4'], S)
-
-        self.mytone(n['C4'], S)
-        self.mytone(n['B3'], S)
-        self.mytone(n['C4'], E)
-        sleep_ms(E)
-        self.mytone(n['F3'], E)
-        self.mytone(n['Ab3'], Q)
-        self.mytone(n['F3'], E + S)
-        self.mytone(n['C4'], S)
-
-        self.mytone(n['LA3'], Q)
-        self.mytone(n['F3'], E + S)
-        self.mytone(n['C4'], S)
-        self.mytone(n['LA3'], H)
-
-        sleep_ms(2 * H)
+        for note, duration in self.melody:
+            self.tone(note, duration)
 
 # Usage
-app = App()
-app.play()
+march = ImperialMarch(buzzer_pin=12, bpm=100)
+march.play()
